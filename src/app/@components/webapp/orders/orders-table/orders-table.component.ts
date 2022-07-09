b0e660fb3bfc4505f956c2ed6a3b9c9498663f7a
Tobi-Osimosu <osimosutobi@gmail.com>
2022-07-09 19:11:36 +0100

    Update

--- src/app/@components/webapp/orders/orders-table/orders-table.component.ts
+++ src/app/@components/webapp/orders/orders-table/orders-table.component.ts
@@ -1,15 +1,96 @@
-import { Component, OnInit } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-orders-table',
   templateUrl: './orders-table.component.html',
-  styleUrls: ['./orders-table.component.scss']
+  styleUrls: ['./orders-table.component.scss'],
 })
-export class OrdersTableComponent implements OnInit {
+export class OrdersTableComponent implements OnInit, OnChanges {
+  displayedColumns: string[] = [
+    'select',
+    'name',
+    'createdDate',
+    'lastModifiedDate',
+    'processStatus',
+    'actions',
+  ];
+  dataSource: MatTableDataSource<any[]> | null = null;
+  selection = new SelectionModel<any>(true, []);
+  totalRecords!: number;
 
-  constructor() { }
+  @Input() data!: any[] | null;
+  @Input() searchValue: any;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.dataSource = new MatTableDataSource(this.data!);
+
+    setTimeout(() => {
+      this.dataSource!.paginator = this.paginator;
+      this.dataSource!.sort = this.sort;
+    });
+
+    if (this.searchValue) {
+      this.search(this.searchValue);
+    }
   }
 
+  ngOnChanges(changes: SimpleChanges): void {}
+
+  search(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource!.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource!.paginator) {
+      this.dataSource!.paginator.firstPage();
+    }
+  }
+
+  onPaginationChange({ pageIndex, pageSize }: any): void {
+    // const skip = pageIndex * pageSize;
+    // const take = pageIndex > 0 ? pageIndex * pageSize : pageSize;
+    // this.loadUsers(skip, take);
+  }
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.totalRecords;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+
+    this.selection.select(...this.dataSource!.data);
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: any): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
+      row.position + 1
+    }`;
+  }
 }
--- src/app/@core/shared/shared.module.ts
+++ src/app/@core/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../styles/material/material.module';
+import { TableLoaderComponent } from './loaders/table-loader/table-loader.component';
 
 const modules = [
   CommonModule,
@@ -10,9 +11,11 @@ const modules = [
   MaterialModule,
 ];
 
+const components = [TableLoaderComponent];
+
 @NgModule({
-  declarations: [],
+  declarations: [components],
   imports: [modules],
-  exports: [modules],
+  exports: [modules, components],
 })
 export class SharedModule {}
