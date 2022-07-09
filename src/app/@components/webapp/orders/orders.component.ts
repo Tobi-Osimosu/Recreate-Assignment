5221bf65971ce6a38d7541abec9dc1cb905934ca
Tobi-Osimosu <osimosutobi@gmail.com>
2022-07-09 23:29:53 +0100

    Update

--- src/app/@components/webapp/orders/orders.component.ts
+++ src/app/@components/webapp/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { catchError } from 'rxjs';
 import { OrdersService } from 'src/app/@core/services/orders.service';
 
 @Component({
@@ -14,20 +15,27 @@ export class OrdersComponent {
   onGetCurrencyDetails(event: any) {
     this.data = null;
 
-    this.ordersService.getCurrencyDetail(event).subscribe((resData: any) => {
-      if (resData) {
-        let tempArray: any[] = [];
+    this.ordersService.getCurrencyDetail(event).subscribe(
+      (resData: any) => {
+        if (resData) {
+          let tempArray: any[] = [];
 
-        for (const property in resData[event]) {
-          tempArray?.push({
-            name: property,
-            value: resData[event][property],
-            date: resData.date,
-          });
+          for (const property in resData[event]) {
+            tempArray?.push({
+              name: property,
+              value: resData[event][property],
+              date: resData.date,
+            });
+          }
+
+          this.data = tempArray;
         }
+      },
+      (error) => {
+        this.data = [];
 
-        this.data = tempArray;
+        window.alert(error.message);
       }
-    });
+    );
   }
 }
